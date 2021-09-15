1380d69d018dc8bf9410386f3e15bd6ac7c3958f
venixGit <69021103+venixGit@users.noreply.github.com>
2021-09-15 17:17:03 -0600

    MenuToggle Modificado

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// import { stickyNav } from './js/componentes';
 import { init } from './js/componentes';
 import './styles.css';
-
-// stickyNav();
 init();
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,12 +1,14 @@
 
 const header      = document.querySelector('header');
-const menuToggle  = document.querySelector('.menuToggle');
+// const menuToggle  = document.querySelector('.menuToggle');
 const navegacion  = document.querySelector('.navigation');
 const home        = document.querySelector('.inicio');
 const about       = document.querySelector('.acerca');
 const skills      = document.querySelector('.habilidad');
 const projects    = document.querySelector('.proyecto');
-
+const menuMovil   = document.querySelector('.toMenu');
+const btn         = document.querySelector('#btn');
+const menu        = document.querySelectorAll('.menu');
 
 /*--------------EVENTOS--------------- */
 const stickyNav = () => {
@@ -15,47 +17,62 @@ const stickyNav = () => {
     });
 }
 
-const menuDesplegable = () => {
-    menuToggle.addEventListener('click', () => {
-        console.log('Esta cargando');
-        menuToggle.classList.toggle('active');
-        navegacion.classList.toggle('active');
+const botonMenu = () => {
+    btn.addEventListener('click', () => {
+        menuMovil.classList.toggle('active');
+        console.log("hola mundo");
     });
 }
-const desactivarMenu =() => {
-    menuToggle.classList.remove('active');
-    navegacion.classList.remove('active');
+
+const menuLinks = () => {
+    menu.forEach((event) => {
+        event.addEventListener('click', () => {
+            menuMovil.classList.toggle('active');
+        });
+    })
 }
 
-
+const menuDesplegable = () => {
+    // menuToggle.addEventListener('click', () => {
+    //     console.log('Esta cargando');
+    //     menuToggle.classList.toggle('active');
+    //     navegacion.classList.toggle('active');
+    // });
+}
+const desactivarMenu =() => {
+    // menuToggle.classList.remove('active');
+    // navegacion.classList.remove('active');
+}
 const ejecutarMenu = () => {
-    if(home.addEventListener){
-        home.addEventListener('click', () => {
-            desactivarMenu();
-        });
-    }
+    // if(home.addEventListener){
+    //     home.addEventListener('click', () => {
+    //         desactivarMenu();
+    //     });
+    // }
     
-    if(about.addEventListener){
-        about.addEventListener('click', () => {
-            desactivarMenu();
-        });
-    }
+    // if(about.addEventListener){
+    //     about.addEventListener('click', () => {
+    //         desactivarMenu();
+    //     });
+    // }
 
-    if(skills.addEventListener){
-        skills.addEventListener('click', () => {
-            desactivarMenu();
-        });
-    }
+    // if(skills.addEventListener){
+    //     skills.addEventListener('click', () => {
+    //         desactivarMenu();
+    //     });
+    // }
 
-    if(projects.addEventListener){
-        projects.addEventListener('click', () => {
-            desactivarMenu();
-        });
-    }
+    // if(projects.addEventListener){
+    //     projects.addEventListener('click', () => {
+    //         desactivarMenu();
+    //     });
+    // }
 }
 
 export const init = () => {
     stickyNav();
     menuDesplegable();
-    ejecutarMenu();
+    botonMenu();
+    menuLinks();
+    // ejecutarMenu();
 }
